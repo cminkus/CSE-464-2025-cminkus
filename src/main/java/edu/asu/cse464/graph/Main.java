99427c87add3f363bb1e0143e2e97c51f2bbae88
cminkus <cminkus@asu.edu>
2025-10-24 16:36:12 -0700

    final: completed all 4 features, tests, and documentation for submission
    # Conflicts:
    #	src/main/java/edu/asu/cse464/graph/Main.java

--- src/main/java/edu/asu/cse464/graph/Main.java
+++ src/main/java/edu/asu/cse464/graph/Main.java
@@ -3,7 +3,7 @@ package edu.asu.cse464.graph;
 public class Main {
     public static void main(String[] args) {
         GraphService gs = new GraphService();
-        gs.parseGraph("examples/sample.dot");
+        gs.parseGraph("examples/sample.dot"); // put a sample there if you want
         System.out.println(gs);
     }
 }
