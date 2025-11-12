af46f5e8f4c690f4fecd1fb6fe382d8df8463371
Cyler Minkus <cminkus@asu.edu>
2025-11-12 14:26:08 -0700

    dfs: add DFS tests

---
+++ src/test/java/edu/asu/cse464/graph/DFSTest.java
@@ -0,0 +1,31 @@
+package edu.asu.cse464.graph;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DFSTest {
+    @Test
+    void reachable() {
+        GraphService g = new GraphService();
+
+        g.addNodes(new String[]{"A","B","C","D"});
+        g.addEdge("A","B");
+        g.addEdge("B","C");
+        g.addEdge("A","D");
+
+        Path p = g.GraphSearch("A", "C");
+        assertNotNull(p);
+        assertEquals("A", p.nodes().get(0));
+        assertEquals("C", p.nodes().get(p.nodes().size() - 1));
+    }
+
+    @Test
+    void unreachable() {
+        GraphService g = new GraphService();
+        g.addNodes(new String[]{"A","B","C"});
+        g.addEdge("A","B");
+
+        Path p = g.GraphSearch("B", "A");
+        assertNull(p);
+    }
+}
